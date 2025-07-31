2059cd54893ec1e270bba59d20c7232c933df37d
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-08-01 02:59:45 +1000

    List of task styling complete

--- todo-app/src/app/components/todo-list/todo-list.ts
+++ todo-app/src/app/components/todo-list/todo-list.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { TodoService, TodoItem } from '../../services/todo';
 import { CommonModule } from '@angular/common';  // For *ngFor
 import { FormsModule } from '@angular/forms';    // For ngModel
-
+import { MatTableModule } from '@angular/material/table'; // ✅ Import Angular Material table
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],  // <-- add these here
+   imports: [CommonModule, FormsModule, MatTableModule],  // ✅ Add MatTableModule
   templateUrl: './todo-list.html',
   styleUrls: ['./todo-list.scss'],
 })
 export class TodoList {
   todos: TodoItem[] = [];
   newTitle = '';
+   displayedColumns: string[] = ['taskTitle', 'action'];  // ✅ Needed for table
 
   constructor(private todoService: TodoService) {
     this.loadTodos();
