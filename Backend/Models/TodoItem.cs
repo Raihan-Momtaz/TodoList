ba79f1f13c8b6b605e912aa2a4aa83a2808155e9
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-07-31 14:32:25 +1000

    Add TodoItem model to define task structure

---
+++ Backend/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+public int TaskId { get; set; }               // id for the task
+public string? TaskTitle { get; set; }        // title of the task
+public bool IsTaskCompleted { get; set; }     // boolean to check if the task is completed or not
+
+}
