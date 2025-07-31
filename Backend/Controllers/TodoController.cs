1c9700f7aeb650a85bc434493ef5d6d04f0b8d62
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-07-31 15:56:07 +1000

    Added controller to handle HTTP requests for TODO operations

---
+++ Backend/Controllers/TodoController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;      
+using TodoApi.Models;                
+using TodoApi.Services;              
+
+namespace TodoApi.Controllers
+{
+    [ApiController]                  
+    [Route("api/[controller]")]     
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoService todoService;  // using the services layer to manege tasks
+
+        
+        public TodoController(TodoService todoService)
+        {
+            this.todoService = todoService;       // Assigns service to the private field
+        }
+
+        // Gets all task
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return todoService.GetAllTasks();     // Retrieve all tasks from service and return them
+        }
+
+        // Gets a task
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> Get(int id)
+        {
+            var todoItem = todoService.GetTaskById(id);   
+            return todoItem == null ? NotFound() : todoItem;  
+        }
+
+        // Adds a task
+        [HttpPost]
+        public ActionResult<TodoItem> Add(TodoItem todoItem)
+        {
+            var createdItem = todoService.AddTask(todoItem);   
+            
+            return CreatedAtAction(nameof(Get), new { id = createdItem.TaskId }, createdItem);
+        }
+
+        // Deletes a task
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var isDeleted = todoService.DeleteTask(id);   
+            return isDeleted ? NoContent() : NotFound(); 
+        }
+    }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
+builder.Services.AddSingleton<TodoApi.Services.TodoService>();
 
 var app = builder.Build();
 
